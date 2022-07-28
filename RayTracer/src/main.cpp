2f0e9631c251b64a3e44c0bab66b7211bcc8e640
Yuvraj Singh <yuvi56789@gmail.com>
2022-07-28 16:12:22 +0530

    Added positionable camera

--- RayTracer/src/main.cpp
+++ RayTracer/src/main.cpp
@@ -40,27 +40,27 @@ int main()
 	const auto aspect_ratio = 16.0 / 9.0;
 	const int image_width = 1280;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 50;
+	const int samples_per_pixel = 300;
 	const int max_depth = 50;
 
-	// camera
-	camera cam;
-
 	// world
+	auto R = cos(pi / 4);
 	hittable_list world;
 
 	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-	//auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+	// auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
 	auto material_center = make_shared<dielectric>(1.5);
 	auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
 	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
 	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-	//hollow glass sphere
+	// hollow glass sphere
 	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
 	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), -0.45, material_center));
 	world.add(make_shared<sphere>(point3(0.0, 0.0, -2.0), 0.5, material_left));
 	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+	// camera
+	camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 20, aspect_ratio);
 
 	// render
 	std::cout << "P3\n"
