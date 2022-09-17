b17324999c86698744659a11a5a059266be4b76d
Yuvraj Singh <yuvi56789@gmail.com>
2022-09-17 23:58:47 +0530

    add basic multithreading

--- RayTracer/src/color.h
+++ RayTracer/src/color.h
@@ -1,11 +1,11 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-#include<iostream>
-#include"vec3.h"
-#include"utility.h"
+#include <iostream>
+#include "vec3.h"
+#include "utility.h"
 
-void write_color(std::ostream& out, color pixel_color, int samples_per_pixel)
+void write_color(std::ostream &out, color pixel_color, int samples_per_pixel)
 {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
@@ -13,14 +13,31 @@ void write_color(std::ostream& out, color pixel_color, int samples_per_pixel)
 
     // gamma correction with gamma=2
     auto scale = 1.0 / samples_per_pixel;
-    r = sqrt(scale*r);
-    g = sqrt(scale*g);
-    b = sqrt(scale*b);
+    r = sqrt(scale * r);
+    g = sqrt(scale * g);
+    b = sqrt(scale * b);
 
     // Write the translated [0,255] value of each color component.
     out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
         << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
         << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
+}
+
+void write_color(std::vector<std::vector<color>> &outputBuffer, color pixel_color, int samples_per_pixel, int i, int j)
+{
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // gamma correction with gamma=2
+    auto scale = 1.0 / samples_per_pixel;
+    r = sqrt(scale * r);
+    g = sqrt(scale * g);
+    b = sqrt(scale * b);
+
+    outputBuffer[i][j].e[0] = static_cast<int>(256 * clamp(r, 0.0, 0.999));
+    outputBuffer[i][j].e[1] = static_cast<int>(256 * clamp(g, 0.0, 0.999));
+    outputBuffer[i][j].e[2] = static_cast<int>(256 * clamp(b, 0.0, 0.999));
 }
 
 #endif
--- RayTracer/src/main.cpp
+++ RayTracer/src/main.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<vector>
+#include<thread>
 #include"color.h"
 #include"ray.h"
 #include"vec3.h"
@@ -6,6 +8,18 @@
 #include"hittable_list.h"
 #include"sphere.h"
 #include"camera.h"
+
+const auto aspect_ratio = 16.0 / 9.0;
+const int image_width = 900;
+const int image_height = static_cast<int>(image_width / aspect_ratio);
+const int samples_per_pixel = 50;
+const int max_depth = 50;
+
+//camera
+camera cam;
+
+//world
+hittable_list world;
 
 color ray_color(const ray& r, const hittable& world, int depth)
 {
@@ -22,11 +36,6 @@ color ray_color(const ray& r, const hittable& world, int depth)
 		return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
 	}
 
-	/*if (world.hit(r, 0, infinity, rec))
-	{
-		return 0.5 * (rec.normal + color(1, 1, 1));
-	}*/
-
 	vec3 unit_direction = unit_vector(r.direction());
 
 	//for vertical gradient
@@ -34,27 +43,10 @@ color ray_color(const ray& r, const hittable& world, int depth)
 	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-int main()
+static void calculateScanline(std::vector<std::vector<color>>& outputBuffer, int startRowNum, int endRowNum)
 {
-	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 400;
-	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 50;
-	const int max_depth = 50;
-	
-	//camera
-	camera cam;
-
-	//world
-	hittable_list world;
-	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));				//main sphere
-	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));		//ground
-
-	//render
-	std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
-	for (int j = image_height - 1; j >= 0; j--)
+	for (int j = startRowNum; j >= endRowNum; j--)
 	{
-		std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
 		for (int i = 0; i < image_width; i++)
 		{
 			color pixel_color(0, 0, 0);
@@ -65,8 +57,73 @@ int main()
 				ray r = cam.get_ray(u, v);
 				pixel_color += ray_color(r, world, max_depth);
 			}
-			write_color(std::cout, pixel_color, samples_per_pixel);
+
+			write_color(outputBuffer, pixel_color, samples_per_pixel, j, i);
 		}
 	}
-	std::cerr << "\nDone.\n";
+}
+
+int main()
+{
+	std::vector<std::vector<color>> outputBuffer(image_height, std::vector<color>(image_width));
+		
+	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+	world.add(make_shared<sphere>(point3(0, 1, -1), 0.5));
+
+	world.add(make_shared<sphere>(point3(1, 1, -1), 0.5));
+	world.add(make_shared<sphere>(point3(-1, 1, -1), 0.5));
+
+	world.add(make_shared<sphere>(point3(-1, 0, -1), 0.5));
+	world.add(make_shared<sphere>(point3(1, 0, -1), 0.5));				//main sphere
+
+	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));			//groundvisual
+
+	//int numThreads = std::thread::hardware_concurrency()*2;
+	int numThreads=12;
+	//int numThreads=16;
+
+	std::vector<std::thread> threadVec;
+	int rowsPerThread = image_height / numThreads;
+
+	//render
+	std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+	int rowCount = image_height - 1;
+	for (int y = 0; y < numThreads; y++)
+	{
+		if (y == numThreads - 1)
+		{
+			std::thread th(calculateScanline, std::ref(outputBuffer), rowCount, 0);
+			threadVec.push_back(std::move(th));
+			rowCount -= rowsPerThread;
+
+		}
+		else
+		{
+			std::thread th(calculateScanline, std::ref(outputBuffer), rowCount, rowCount - rowsPerThread);
+			threadVec.push_back(std::move(th));
+			rowCount -= rowsPerThread;
+		}
+	}
+
+	int count = 1;
+	for (auto& t : threadVec)
+	{
+		if (t.joinable())
+		{
+			t.join();
+		}
+		std::cerr << "\rThreads Done:  " << count << " " << std::flush;
+		count++;
+	}
+
+	for (int j = image_height - 1; j >= 0; j--)
+	{
+		for (int i = 0; i < image_width; i++)
+		{
+			std::cout << outputBuffer[j][i].e[0] << " " << outputBuffer[j][i].e[1] << " " << outputBuffer[j][i].e[2] << "\n";
+		}
+	}
+	
+	std::cerr << "\ndone.\n";
 }
